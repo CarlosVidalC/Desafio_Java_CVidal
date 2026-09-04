b3402b3ec33055823b9abb247dae7855d8bab267
Carlos_Vidal <cari.vidal@duocuc.cl>
2026-09-03 21:14:34 -0400

    JAVA-003 - Agregar constructor de Estudiante

--- Desafio_Cvidal/src/Estudiante.java
+++ Desafio_Cvidal/src/Estudiante.java
@@ -3,7 +3,7 @@ public class Estudiante {
     private String carrera;
     private int edad;
 
-    public Estudiante(String nombre, int edad, String carrera) {
+    public Estudiante(String nombre, String carrera, int edad) {
         this.nombre = nombre;
         this.carrera = carrera;
         this.edad = edad;
